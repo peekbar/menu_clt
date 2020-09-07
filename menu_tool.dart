077862ba38992e29c2d8e8239693c2e070cd5b07
jonaspoxleitner <jonas.pox@gmail.com>
2020-09-07 15:45:29 +0200

    added basic functionality

--- menu_tool.dart
+++ menu_tool.dart
@@ -1,21 +1,64 @@
-void main(List<String> arguments) {
-  // if argument init: init this program
-  // create database: don't do this when there is a database in database dir
-  // don't run upgade as the database is empty
+import 'dart:io';
+import 'dart:async';
 
-  // if argument upgrade: get new web from github
-  // save the version of /web/info.json
-  // remove the web folder
-  // dowload the new web folder from git
-  // if the version is the same: do nothing
-  // if version is different: run over all menus in the database
-  // create new dir for new menus if necessary
-  // deploy them to the server
+import 'package:args/args.dart';
 
-  // if argument upgrade _company_name_: check for of the menu
-  // compare info.json from web dir with info.json from menus/_company_name_ dir
-  // if dir is not there: new company
-  // create new if necessary and then deploy
+import 'package:mysql1/mysql1.dart';
 
-  // if no argument or help: print help instructions for the use of this program
+void main(List<String> arguments) async {
+  exitCode = 0; // presume success
+
+  var parser = ArgParser();
+  var results = parser.parse(arguments);
+
+  if (results.rest.isEmpty) {
+    print('Sorry, but you have to add a command.');
+    exit(1);
+  }
+
+  switch (results.rest[0]) {
+    case 'i':
+    case 'init':
+      print('init');
+      new Directory('menus/company_name')
+          .create(recursive: true)
+          .then((Directory directory) {
+        print(directory.path);
+      });
+      // if argument init: init this program
+      // create database: don't do this when there is a database in database dir
+      // don't run upgade as the database is empty
+      break;
+    case 'u':
+    case 'upgrade':
+      if (results.rest.length > 1) {
+        print('Only upgrading \'' +
+            results.rest[1].toString() +
+            '\' to a new version.');
+        // if argument upgrade _company_name_: check for of the menu
+        // compare info.json from web dir with info.json from menus/_company_name_ dir
+        // if dir is not there: new company
+        // create new if necessary and then deploy
+      } else {
+        print('Upgrading all menus to a new version.');
+        // if argument upgrade: get new web from github
+        // save the version of /web/info.json
+        // remove the web folder
+        // dowload the new web folder from git
+        // if the version is the same: do nothing
+        // if version is different: run over all menus in the database
+        // create new dir for new menus if necessary
+        // deploy them to the server
+      }
+      break;
+    case 'h':
+    case 'help':
+      print('Welcome to the help center.');
+      exit(1);
+      break;
+    default:
+      print('Sorry, but this command is not supported.');
+      print('Please use \'help\' for more information about this program.');
+      exit(1);
+  }
 }
